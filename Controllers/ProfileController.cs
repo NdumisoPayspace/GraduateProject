c26305a1849eaf1fa8bc275faf8d9956cf272d9f
ndumisoz <ndumisoz@NDUMISOZ-SSD>
2023-03-06 13:50:30 +0200

    Created Profile View.

---
+++ Controllers/ProfileController.cs
@@ -0,0 +1,15 @@
+﻿using GraduateProject.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GraduateProject.Controllers
+{
+    public class ProfileController : Controller
+    {
+        public IActionResult Index()
+        {
+            var profile = new ProfileViewModel();
+
+            return View(profile);
+        }
+    }
+}
---
+++ Models/ProfileViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel;
+using System.Diagnostics.Metrics;
+using System.Xml.Linq;
+
+namespace GraduateProject.Models
+{
+    public class ProfileViewModel
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        [DisplayName("Date of Birth")]
+        public string DOB { get; set; }
+        public int Age { get; set; }
+        public string Country { get; set; }
+        public string City { get; set; }
+        public ProfileViewModel()
+        {
+            Name = "Melo";
+            Surname = "Ball";
+            DOB = "2001/08/22";
+            Age = 21;
+            Country = "America";
+            City = "Anahiem";
+        }
+    }
+
+
+}
